307982925bed48ae6b0c9d2465703ca13f8f2ac7
astone4831 <137156673+astone4831@users.noreply.github.com>
2023-06-20 04:43:55 -0400

    first_commit
    just playing with github

---
+++ hello.jl
@@ -0,0 +1,30 @@
+using Pkg
+Pkg.add("MusicXML")
+Pkg.add("EzXML")
+
+using MusicXML
+using EzXML
+
+#file = "C:\\Users\\stone\\Desktop\\julia_music\\52nd Street Theme.musicxml"
+file = "C:\\Users\\stone\\Downloads\\Give A Little Bit.musicxml"
+
+score = readmusicxml_partial(file)
+println(score)
+
+println(nodecontent.(findall("//work-title", score)))
+
+chord = []
+for harmony in nodecontent.(findall("//root-step", score))
+    push!(chord,harmony)
+end
+alter = []
+for harmony in nodecontent.(findall("//root-alter", score))
+    push!(alter,harmony)
+end
+flavor = []
+for harmony in nodecontent.(findall("//kind/text()", score))
+    push!(flavor, harmony)
+end
+println(chord)
+println(alter)
+println(flavor)
